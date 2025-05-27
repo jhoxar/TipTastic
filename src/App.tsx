38166f696c56127120eef6bba08a756b94d12043
jhoxar <jhon98-ra@hotmail.com>
2025-05-27 14:26:25 -0500

    🧱 Added main layout containers – building the foundation!

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,18 @@ function App() {
      <header className="bg-teal-400 py-5">
       <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
      </header>
+
+     <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+
+        <div>
+          <h2>menu</h2>
+        </div>
+
+        <div>
+          <h2>consumo</h2>
+        </div>
+      
+     </main>
     </>
   )
 }
