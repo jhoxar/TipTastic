0a529bbe1f320bf35d48fcf54d0e1cbc9eef38a9
jhoxar <jhon98-ra@hotmail.com>
2025-05-27 13:25:15 -0500

    🧾 Set up core structure with TypeScript: added menuItems DB, types, and app skeleton

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { menuItems } from "./data/db"
+
+function App() {
+  console.log(menuItems);
+  
+  return (
+    <>
+     <header className="bg-teal-400 py-5">
+      <h1 className="text-center text-4xl font-black">Calculadora de propinas y consumo</h1>
+     </header>
+    </>
+  )
+}
+
+export default App
