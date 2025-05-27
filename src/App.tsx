53f34042b5679264c8024d00d85a70a38c782b54
jhoxar <jhon98-ra@hotmail.com>
2025-05-27 14:46:11 -0500

    📋 Listed menu products – the feast is getting real!

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,22 @@ function App() {
 
      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
 
-        <div>
-          <h2>menu</h2>
-          {menuItems.map(item => 
+        <div className="p-5">
+
+          <h2 className="text-4xl font-black">menu</h2>
+
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => 
             <MenuItem 
               key={item.id}
+              item = {item}
             />
           )}
+
+          </div>
+
+          
+
         </div>
 
         <div>
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,6 +1,18 @@
-export default function MenuItem() {
+
+import type { MenuItem} from "../types"
+
+type MenuItemProps = {
+    item: MenuItem
+}
+
+export default function MenuItem({item}: MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <button
+    className="border-2 border-teal-400 w-full p-3 flex justify-between hover:bg-teal-200"
+    >
+    <p>{item.name}</p>
+    <p className="font-black">${item.price}</p>
+    </button>
   )
 }
 
